606fb67bd0689280f14b507eed48df93530e1577
unknown <teaguehannam@gmail.com>
2024-02-14 05:22:37 -0700

    interation 🫠

--- website/src/utils/bg.js
+++ website/src/utils/bg.js
@@ -4,8 +4,23 @@ export const DEFAULT_BG = 'https://dchqsxvlazultvsphmau.supabase.co/storage/v1/o
 export const load_bg = (new_bg = false) => {
   if (new_bg) window.localStorage.setItem('BG_URL', new_bg)
   let url = localStorage.getItem('BG_URL') || DEFAULT_BG
-  document.body.style = `
-    background: url(${url}) no-repeat center center fixed;
-    background-size:cover;
-  `;
+  if (document.body.querySelector('video')) document.body.querySelector('video').remove()
+
+  if (url.endsWith('.mp4')) {
+    let v = document.createElement('video');
+    v.setAttribute('autoplay', 'true');
+    v.setAttribute('muted', '');
+    v.setAttribute('loop', '');
+    let s = document.createElement('source');
+    s.setAttribute('src', url);
+    s.setAttribute('type', 'video/mp4');
+    v.appendChild(s);
+    document.body.appendChild(v);
+  } else {
+    document.body.style = `
+      background: url(${url}) no-repeat center center fixed;
+      background-size:cover;
+    `;
+  }
 }
+
